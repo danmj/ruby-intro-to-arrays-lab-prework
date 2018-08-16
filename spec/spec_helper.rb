be0dccd616e7d2d35ea9475086df4fc8e44ad4f6
danmj <daniel.jung@stern.nyu.edu>
2018-08-16 13:30:25 +0000

    automatically backed up by learn

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,8 +10,6 @@ RSpec.configure do |config|
     @my_two_aray = [0, 1]
   end
   
-  taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
-  
   def first_element(taylor_swift)
     taylor_swift[0]
 end
