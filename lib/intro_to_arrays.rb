92f079c3d5dfebe170f5f58a49750fd8bc7f0146
danmj <daniel.jung@stern.nyu.edu>
2018-08-16 13:39:40 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -1,3 +1,5 @@
-taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
+@taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
 
-south_east_asia = ["Thailand", "Cambodia", "Singapore", "Myanmar"]
+@south_east_asia = ["Thailand", "Cambodia", "Singapore", "Myanmar"]
+
+@programming_languages = ["Ruby", "Javascript", "Python", "C++", "Java", "Lisp", "PHP", "Clojure"]
