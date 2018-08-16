158604c5d64ede04b7ef576ebf5750eb83683dfa
danmj <daniel.jung@stern.nyu.edu>
2018-08-16 13:30:00 +0000

    automatically backed up by learn

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,6 +13,5 @@ RSpec.configure do |config|
   taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
   
   def first_element(taylor_swift)
-    
-
+    taylor_swift[0]
 end
